9794a07d92def60f06fcf0d91aacbc1bce3eb1f8
Gleb Azeev <azeevg@gmail.com>
2016-04-06 00:40:06 +0300

    Change logic of classes

--- src/main/java/CashMachineMessageHandler.java
+++ src/main/java/CashMachineMessageHandler.java
@@ -13,11 +13,15 @@ public class CashMachineMessageHandler {
 
     // package visibility only for testing
 
+    String handle(@NotNull final String input) {
+        return "res";
+    }
+
     String put(@NotNull final Integer denomination, @NotNull final Integer count) {
         return "res";
     }
 
-    String get(final Integer amount) {
+    String get(@NotNull final Integer amount) {
         return "res";
 
     }
@@ -31,6 +35,9 @@ public class CashMachineMessageHandler {
         return "res";
     }
 
+    String help() {
+        return "help";
+    }
 
 
 }
--- src/main/java/CashMachineRunner.java
+++ src/main/java/CashMachineRunner.java
@@ -1,15 +1,10 @@
-import java.io.InputStream;
-import java.io.OutputStream;
-
 public class CashMachineRunner {
 
     public static void main(String[] args) {
         final Integer[] denominations = {5000, 1000, 500, 100, 50, 25, 10, 5, 3, 1};
-        final InputStream inputStream = System.in;
-        final OutputStream outputStream = System.out;
         final CashMachineMessageHandler cashMachineMessageHandler;
 
-        cashMachineMessageHandler = new CashMachineMessageHandler(inputStream, outputStream, denominations);
+        cashMachineMessageHandler = new CashMachineMessageHandler(denominations);
         cashMachineMessageHandler.run();
     }
 }
